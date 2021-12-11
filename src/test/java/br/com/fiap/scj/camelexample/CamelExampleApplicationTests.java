b0a2fd22abcd1872027d2b0ddf405ae83bf90c3c
Logon PF Fiap <logonpflocal@fiap.com.br>
2021-12-11 12:10:47 -0300

    versão inicial

---
+++ src/test/java/br/com/fiap/scj/camelexample/CamelExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.scj.camelexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
